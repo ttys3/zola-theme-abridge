c0c79d8be1235aaae30b3262791f1132128db972
Jieiku <106644+Jieiku@users.noreply.github.com>
2022-08-19 12:07:45 -0700

    Close #89, stork/tinysearch json/toml generated only if configured.

--- static/search_facade.js
+++ static/search_facade.js
@@ -2,8 +2,8 @@ var baseUrl = document.querySelector("meta[name='base']").getAttribute("content"
 if (baseUrl.slice(-1) == "/") {
     baseUrl = baseUrl.slice(0, -1);
 }
-var sha256='f0d8483e2a6f244c830fb75f3f593337ad4ee9c509fa5ee630b63ab86339a322';
-var sha384='Rlj4K5jjLvHBI8PY8deIstHEL3yP4u5fvAfPZejNUqvFUc1Y9gD+liL+c9by9bp5';
+var sha256='a4b6ea92ca8737f338fc8c78fcb62621b983cced9a0507aa864205bd7aa807fd';
+var sha384='M7JvR78mOoM0fSQOc5ypscN72SzRvbViAugr4voH6PYrTyS/w6H0lLBgOKJaKfQl';
 function loadSearchNow() {
     var loadSearch = document.createElement('script');
     loadSearch.src = baseUrl + '/search_bundle.min.js?h=' + sha256;
